3c69522bee924c155470d06e801ff5630fcf0e1f
Arman-Kabir <armankabir8@gmail.com>
2023-05-12 16:42:32 +0600

    basic setup done

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 5000;
+
+//database connection
+async function bootstrap() {
+  try{
+    await mongoose.connect("mongodb://127.0.0.1:27017/practice-mongoose");
+    console.log(`Database connection successful`);
+    }catch(err){
+        console.log('Failed to connect database',err);
+    }
+  }
+
+bootstrap();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
