514a6582aea5d1653cf353edbdb8e6f4fdf84765
Arman-Kabir <armankabir8@gmail.com>
2023-05-12 11:18:34 +0600

    basic dependencies setup done

---
+++ mod6-notes.js
@@ -0,0 +1,24 @@
+/*
+    #1
+    ----
+        Interface Schema Model Database Query
+
+
+    #2  Install express , typescript, mongoose , ts-node-dev
+    --------------------------------------------------------
+    i will use my computer as a server
+    google-> mongodb download -> MongoDb Community server
+
+    *after installing- add path variable of the bin folder
+    -------
+    *npm init
+    ----installing ts
+    npm install typescript --save-dev
+    --------install express
+    npm install express --save
+    -----install mongoose
+    npm install mongoose --save
+    
+
+
+*/
