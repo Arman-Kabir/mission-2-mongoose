504e32f7bb744e9efcac71a6842e2f7f1489bacb
Arman-Kabir <armankabir8@gmail.com>
2023-05-15 12:30:27 +0600

    gui setup

---
+++ mod7-notes.js
@@ -0,0 +1,21 @@
+/*
+    #1: Overview Of previous Module ,Install Studio 3T , No SQL Booster
+--------------------------------------------------------------------------
+        save-> save() method is in mongoose -> mongoose does it with data validate using-> schema
+        mongoose doesn't  use mongoDB insert or insertMany as that inserts any type of data
+
+        except insert and insertMany of mongoDB query -> all other queries work in mongoose
+
+        ***Download practice data ----> https://github.com/Apollo-Level2-Web-Dev/mongoose-practice-data/blob/main/practice-data.json
+
+        *******Queries********
+        *show databases
+        *use practice-mongoose
+        *db.getCollection('practice').find()
+        *db.getCollection('practice').find().limit(2)
+
+            ***GUI Tools***
+            *Studio 3T
+            *NoSql Booster
+
+*/
